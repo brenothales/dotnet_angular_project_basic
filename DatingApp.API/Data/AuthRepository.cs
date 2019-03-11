b9c2a09f0719866480be6deccf9c7e8359386cb2
Breno Thales <breno.thales@gmail.com>
2019-03-10 21:27:41 -0300

    Create Resource for token JWT

--- DatingApp.API/Data/AuthRepository.cs
+++ DatingApp.API/Data/AuthRepository.cs
@@ -1,73 +1,69 @@
-using System;
-using System.Threading.Tasks;
 using DatingApp.API.Models;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace DatingApp.API.Data
-{
-  public class AuthRepository : IAuthRepository
-  {
-    private readonly DataContext _context;
+namespace DatingApp.API.Data{
 
-    public AuthRepository(DataContext context)
-      {
-      _context = context;
-    }
-    public async Task<User> Login(string username, string password)
-    {
-        var user = await _context.Users.FirstOrDefaultAsync(x => x.Username == username);
+    public class AuthRepository : IAuthRepository {
 
-        if(user == null)
-            return null;
+        private readonly DataContext _context;
 
-        if(!VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt))
-            return null;
+        public AuthRepository(DataContext context) {
+            _context = context;
+        }
 
-        return user;
-    }
+        public async Task<User> Login(string username, string password) {
 
-    private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
-    {
-      using( var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
-      {
-         var computedSalt = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
-         for (int i = 0; i < computedSalt.Length; i++)
-         {
-             if (computedSalt[i] != passwordHash[i]) return false;
-         }
-      }
-      return true;
-    }
+            User user = await _context.Users.FirstOrDefaultAsync(u => u.Username == username);
 
-    public async Task<User> Register(User user, string password)
-    {
-      byte[] passwordHash, passwordSalt;
-      createPasswordHash(password, out passwordHash, out passwordSalt);
-      user.PasswordHash = passwordHash;
-      user.PasswordSalt = passwordSalt;
+            if (user == null)
+                return null;
 
-      await _context.Users.AddAsync(user);
-      await _context.SaveChangesAsync();
+            if (!verifyPasswordHash(password, user.PasswordHash, user.PasswordSalt))
+                return null;
 
-      return user;
-    }
+            return user;
+        }
 
-    private void createPasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
-    {
-      using( var hmac = new System.Security.Cryptography.HMACSHA512() )
-      {
-          passwordHash = hmac.Key;
-          passwordSalt = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+        public async Task<User> Register(User user, string password) {
 
-      }
-    }
+            byte[] passwordHash, passwordSalt;
+            CreatePasswordHash(password, out passwordHash, out passwordSalt);
 
-    public async Task<bool> UserExists(string username)
-    {
-        if (await _context.Users.AnyAsync(x => x.Username == username))
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+
+            return user;
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt) {
+
+            using (HMACSHA512 hmac = new HMACSHA512()) {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        public async Task<bool> UserExists(string username) {
+            return await _context.Users.AnyAsync(u => u.Username == username);
+        }
+
+        private bool verifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt) {
+
+            using (HMACSHA512 hmac = new HMACSHA512(passwordSalt)) {
+
+                byte[] computeHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+                for (int i = 0; i < computeHash.Length; ++i) {
+                    if (computeHash[i] != passwordHash[i]) return false;
+                }
+            }
             return true;
-
-        return false;
+        }
     }
-  }
 }
--- DatingApp.API/Dto/UserForRegisterDto.cs
+++
@@ -1,8 +0,0 @@
-namespace DatingApp.API.Dto
-{
-    public class UserForRegisterDto
-    {
-        public string Username { get; set; }
-        public string Password { get; set; }
-    }
-}
