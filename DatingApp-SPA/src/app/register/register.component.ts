39ae9d06a775c7c6d4b67886ff3eb75c30169f24
Breno Thales <breno.thales@gmail.com>
2019-03-13 00:35:31 -0300

    Create function to authenciation via angular

---
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -0,0 +1,25 @@
+import { AuthService } from './../_services/auth.service';
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  @Input() valuesFromHome: any;
+  model: any = {};
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  register() {
+    this.authService.register(this.model).subscribe(() => {
+      console.log('registration successful');
+    }, error => {
+      console.log(error);
+    });
+  }
+
+}
